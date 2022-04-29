8a871b0b486c4952d59c9af43f6880379a8a4fe8
thulasiram321 <vijaikrishnan321@gmail.com>
2022-04-29 18:27:09 +0530

    aprilemply

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,33 @@
+package org.emp;
+
+public class Employee {
+
+	public void empId() {
+		System.out.println("thulasi97");	
+	}
+	public void empName() {
+		System.out.println("thulasiram");	
+	}
+	public void empDob() {
+		System.out.println("28nov");
+	}
+	public void empPhone() {
+		System.out.println("9095442267");
+	}
+	public void empEmail() {
+	    System.out.println("thulasiela12@gmail.com");
+	}
+	public void empAddress() {
+		System.out.println("thanjavur");
+	}
+	public static void main(String[] args) {
+		Employee E = new Employee();
+		E.empId();
+		E.empName();
+		E.empDob();     
+		E.empPhone();
+		E.empEmail();
+		E.empAddress();
+	}	
+	}
+
